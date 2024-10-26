03ab66d8716d842b162b7992aa316a947eb12524
TheHuntOfCode <quanten.verglasung-0w@icloud.com>
2024-10-26 23:43:36 +0200

    Initial Commit

---
+++ src/com/spielwiese/meinlernprojekt/Main.java
@@ -0,0 +1,10 @@
+package com.spielwiese.meinlernprojekt;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 3;
+
+        System.out.println(number);
+
+    }
+}
